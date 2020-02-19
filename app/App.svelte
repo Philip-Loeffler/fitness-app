a5e5f807534b35dcfffa2a845c7a125552005b3d
knipferrc <knipferrc@gmail.com>
2020-02-19 18:44:38 -0500

    structure cleanup

---
+++ app/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  const cardio = ["swimming", "biking", "running", "boxing", "HIIT"];
+
+  let randomWork;
+
+  function tapForCardio() {
+    randomWork = cardio[Math.floor(Math.random() * cardio.length)];
+  }
+</script>
+
+<page>
+  <actionBar title="My App" icon="" />
+  <stackLayout class="p-20">
+    <label text="Tap the button" class="h1 text-center" />
+    <button text="TAP" on:tap={tapForCardio} class="-primary -active" />
+    <label text={randomWork} class="h2 text-center" textWrap="true" />
+  </stackLayout>
+</page>
